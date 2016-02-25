36ceba62994cca5f78f2de0969553daa236ce029
Tibor <tkoroknai2@gmail.com>
2016-02-25 14:53:27 +0100

    initial

---
+++ mocks/CMakeFiles/RuntimeErrorStub.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RuntimeErrorStub.dir/RuntimeErrorStub.c.o"
+  "../lib/libRuntimeErrorStub.pdb"
+  "../lib/libRuntimeErrorStub.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/RuntimeErrorStub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ tests/CMakeFiles/sprintfTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sprintfTest.dir/sprintfExample/sprintfTest.cpp.o"
+  "../lib/libsprintfTest.pdb"
+  "../lib/libsprintfTest.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sprintfTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
